8cee0677c8873885975e2e106c905892e73e8a58
Ares DoDox <luyn14@gmail.com>
2019-02-13 15:49:39 +0700

    nodemon & lowdb

--- index.js
+++ index.js
@@ -3,6 +3,19 @@ var bodyParser = require('body-parser');
 var app = express();
 var port = 3000;
 
+//lowdb
+var low = require('lowdb');
+var FileSync = require('lowdb/adapters/FileSync');
+
+var adapter = new FileSync('db.json');
+var db = low(adapter);
+
+
+// Set some defaults (required if your JSON file is empty)
+db.defaults({ users: [] })
+  .write()
+
+
 app.set('view engine', 'pug');
 app.set('views', './views');
 
@@ -29,18 +42,18 @@ app.get('/',function(req, res){
 
 app.get('/users',function(req, res){
     res.render('users/index', {
-        users: users
+        users: db.get('users').value()
     });
 });
 
 //Tìm kiếm
 app.get('/users/search', function(req, res) {
     var q = req.query.q;
-    var matchedUsers = users.filter(function(user){
+    var matchedUsers = db.get('users').value().filter(function(user){
         return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
     });
-    // toLowerString()
-    // indexOf(q)
+    // toLowerString() Chuyển chuỗi hiện tại thành chuỗi chữ thường
+    // indexOf(q) Kiểm tra trong chuổi tồn tại biến q không (không tồn tại trả về giá trị -1)
     res.render('users/index', {
         users: matchedUsers
     });
@@ -54,6 +67,6 @@ app.get('/users/create', function(req,res){
 });
 app.post('/users/create', function(req,res){
     // console.log(req.body);
-    users.push(req.body);
-    res.redirect('/users');
+    db.get('users').push(req.body).write(); // 
+    res.redirect('/users'); //Điều hướng sang url khác
 });
