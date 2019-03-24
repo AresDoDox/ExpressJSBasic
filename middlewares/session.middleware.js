d1ed2f2c7925cbc561a4041f01f7971d5b8f46c1
Ares DoDox <luyn14@gmail.com>
2019-03-25 01:29:24 +0700

    return numberProducts Cart (header nav)

--- middlewares/session.middleware.js
+++ middlewares/session.middleware.js
@@ -3,6 +3,7 @@ const shortid = require('shortid');
 var db = require('../db');
 
 module.exports = function(req, res, next){
+    var numProducts = 0;
     if (!req.signedCookies.sessionId){
         var sessionId = shortid.generate();
         res.cookie('sessionId', sessionId, {
@@ -13,7 +14,17 @@ module.exports = function(req, res, next){
         db.get('sessions').push({
             id: sessionId
         }).write();
+    }else{
+        var sessionId =req.signedCookies.sessionId;
+        var sessionCart = db.get('sessions').find({id: sessionId}).value();
+        if (sessionCart.cart){
+            for (const countProduct in sessionCart.cart) {
+                numProducts = numProducts + sessionCart.cart[countProduct];
+            }
+        }
     }
-
+    
+    
+    res.locals.numProducts = numProducts;
     next();
 }
